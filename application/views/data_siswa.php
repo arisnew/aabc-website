e1a324e8f807d57fdde219b98724311cff0d2431
Arisnew <aris.new2011@gmail.com>
2020-04-20 05:34:28 +0700

    file ci kemarin

---
+++ application/views/data_siswa.php
@@ -0,0 +1,25 @@
+<h4>Data Siswa</h4>
+<table border="1" width="80%">
+	<tr>
+		<th>No</th>
+		<th>Nama</th>
+		<th>Kelas</th>
+		<th>Keterangan</th>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td>Agus</td>
+		<td>IPA</td>
+		<td>Masuk</td>
+	</tr>
+</table>
+
+
+<div style="background-color: lightblue; padding: 10px;">
+	<?php
+
+	if ($data_nilai) {
+		echo $data_nilai;
+	}
+	?>
+</div>
---
+++ application/views/template/footer.php
@@ -0,0 +1,9 @@
+
+<!-- footer -->
+	
+	<p class="footer">
+		<hr>
+		&copy; 2020 AABC Team - All rights reserverd.
+	</p>
+</body>
+</html>
