fa7762fe23583a2018a38af6e813e981ab98b66e
Arisnew <aris.new2011@gmail.com>
2020-04-28 06:59:09 +0700

    CRUD AJAX part 2

--- application/controllers/Ajaxlevel.php
+++ application/controllers/Ajaxlevel.php
@@ -18,7 +18,8 @@ class Ajaxlevel extends CI_Controller {
 	public function index()
 	{
 		$extra_js = $this->load->view('ajaxlevel/extra_js', NULL, TRUE);
-		$data_level = $this->levelmodel->read_data();
+		$data_level = null; //karena via ajax
+		// $this->levelmodel->read_data();
 		$this->load->view('template/header');
 		$this->load->view('ajaxlevel/index', array('levels' => $data_level));
 		$this->load->view('template/footer', array('extra_js' => $extra_js));
@@ -58,5 +59,95 @@ class Ajaxlevel extends CI_Controller {
 			'code' => $code,
 			'msg' => $msg
 		));
+	}
+
+	/* data level dalam bentuk json u/ datatable*/
+	public function source_data()
+	{
+		$data = array();
+
+		//data level
+		$data_level = $this->levelmodel->read_data();
+		if ($data_level) {
+			foreach ($data_level as $row) {
+				$data[] = array(
+					'level_id' => $row->level_id,
+					'level_name' => $row->level_name,
+					'description' => $row->description,
+					'is_active' => $row->is_active,
+					'option' => '
+						<a href="#'.$row->level_id.'" class="btn-edit btn btn-sm btn-warning"> Edit</a>
+						<a href="#'.$row->level_id.'" class="btn-delete btn btn-sm btn-danger"> Delete</a>
+					'
+				);
+			}
+		}
+		//output json
+		echo json_encode(array('data' => $data));
+	}
+
+	public function get_data()
+	{
+		$code = 0;
+		$level_id = $this->input->post('id');
+		// data level berdasarkan id
+		$level = $this->levelmodel->get_data($level_id);
+		if ($level) {
+			$code = 1;
+		}
+
+		//output dlm bentuk json
+		echo json_encode(array('code' => $code, 'object' => $level));
+	}
+
+	public function update()
+	{
+		$code = 0;
+		$msg = '';
+		//validasi
+		// setting rule validasi
+		$this->form_validation->set_rules('id-edit', 'ID', 'required');
+		$this->form_validation->set_rules('level-name-edit', 'Level Name', 'required');
+		$this->form_validation->set_rules('description-edit', 'Description', 'required');
+		//cek validasi
+		if ($this->form_validation->run() == FALSE) {
+			//jika tidak valid
+			$msg = validation_errors();
+		} else {
+			//prepare data (mapping data) \
+			$data = array(
+				'level_name' => $this->input->post('level-name-edit'),
+				'description' => $this->input->post('description-edit'),
+				'is_active' => $this->input->post('status-edit')
+			);
+			//proses update
+			$update = $this->levelmodel->update_data($this->input->post('id-edit'), $data);
+			//cek berhasil atau tidak
+			if ($update) {
+				$code = 1;
+			} else {
+				$msg = 'Update data tidak berhasil! Cek data tersebut.';
+			}
+		}
+
+		//output dalam bentuk json
+		echo json_encode(array(
+			'code' => $code,
+			'msg' => $msg
+		));
+	}
+
+	public function delete_data()
+	{
+		$code = 0;
+		$level_id = $this->input->post('id');
+		// data level berdasarkan id
+		$delete = $this->levelmodel->delete_data($level_id);
+		if ($delete) {
+			$code = 1;
+		}
+
+		//output dlm bentuk json
+		echo json_encode(array('code' => $code));
 	}
 }
--- application/views/ajaxlevel/index.php
+++ application/views/ajaxlevel/index.php
@@ -1,43 +1,24 @@
 <h3>Data Level <small>AJAX</small></h3>
+<div id="loading-table-level" class="text-center" style="display: none;">
+	<img src="<?php echo base_url('asset/images/ajax-loader.gif');?>">
+</div>
+
 <a href="#" data-toggle="modal" data-target="#modal-form-add-level" class="btn btn-success pull-right"> Tambah Data</a>
 
 <table id="table-level-ajax" class="table table-sm table-hover">
 	<thead>
 		<tr>
-			<th>No</th>
 			<th>Level Name</th>
 			<th>Description</th>
 			<th>Status</th>
 			<th>Option</th>
 		</tr>
 	</thead>
-	<tbody>
-		<?php
-		if ($levels) {
-			$no = 1;
-			foreach ($levels as $row) {
-				echo '
-				<tr>
-					<td>'.$no.'</td>
-					<td>'.$row->level_name.'</td>
-					<td>'.$row->description.'</td>
-					<td>'.$row->is_active.'</td>
-					<td>
-						<a href="'.base_url('level/edit/' . $row->level_id).'" class="btn btn-sm btn-warning"> Edit</a>
-						<a onclick="return confirm(\'Yakin hapus data level '.$row->level_name.' ?!\');" href="'.base_url('level/delete/' . $row->level_id).'" class="btn btn-sm btn-danger"> Delete</a>
-					</td>
-				</tr>
-				';
-
-				$no++;
-			}
-		}
-		?>
-	</tbody>
+	<tbody></tbody>
 
 </table>
 
-<!-- modal -->
+<!-- modal add -->
 <div class="modal fade" id="modal-form-add-level" tabindex="-1" role="dialog" 
 	aria-labelledby="formAddModalLabel" aria-hidden="true">
 	<div class="modal-dialog" role="document">
@@ -71,6 +52,46 @@
 			<div class="modal-footer">
 				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
 				<button type="button" id="btn-save-add-level" class="btn btn-primary">Save changes</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- modal edit -->
+<div class="modal fade" id="modal-form-edit-level" tabindex="-1" role="dialog" 
+	aria-labelledby="formEditModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="formEditModalLabel">Edit Data Level</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<form id="form-edit-level" method="POST">
+					<div id="loading-edit-level" class="text-center" style="display: none;">
+						<img src="<?php echo base_url('asset/images/ajax-loader.gif');?>">
+					</div>
+					<div class="form-group">
+						<label for="level-name-edit">Level Name :</label>
+						<input type="hidden" name="id-edit" id="id-edit" value="">
+						<input type="text" class="form-control" id="level-name-edit" name="level-name-edit" required="">
+					</div>
+					<div class="form-group">
+						<label for="description-edit">Description :</label>
+						<textarea class="form-control" id="description-edit" name="description-edit"></textarea>
+					</div>
+					<div class="form-group">
+						<label for="status-edit">Status :</label>
+						<input type="radio" name="status-edit" id="status-edit-active" value="Active" checked=""> Active 
+						<input type="radio" name="status-edit" id="status-edit-nonactive" value="Nonactive"> Nonactive
+					</div>
+				</form>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				<button type="button" id="btn-save-edit-level" class="btn btn-primary">Update</button>
 			</div>
 		</div>
 	</div>
